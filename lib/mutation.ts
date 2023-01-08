f084f589c0c3818b92a4ea8f1e1d8f7a0f044b13
Femi Ajayi <femiajayi705@gmail.com>
2023-01-08 20:52:29 +0100

    feat: Created api helper libs

---
+++ lib/mutation.ts
@@ -0,0 +1,9 @@
+import fetcher from "./fetchers";
+
+const auth = (mode: 'signin' | 'signup', body: {
+    email: string; password: string
+}) => {
+    return fetcher(`/${mode}`, body)
+}
+
+export default auth
