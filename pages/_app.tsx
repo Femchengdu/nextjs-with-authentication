7170385fab227c69e76894cfd3aaaf29df03d5c7
Femi Ajayi <femiajayi705@gmail.com>
2023-01-08 06:33:35 +0100

    feat: Added chakra UI and page layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '../styles/globals.css'
+import 'reset-css'
+import {ChakraProvider, extendTheme} from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import PlayerLayout from "../components/pageLayout.tsx"
 
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "*f5f5f5"
+    }
+  },
+  components: {
+    Button: {
+      variants: {
+        link:{
+          ":focus": {
+            outline: "none"
+          }
+        }
+      }
+    }
+  }
+})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ChakraProvider theme={theme}>
+    <PlayerLayout>
+      <Component {...pageProps} />
+    </PlayerLayout>
+  </ChakraProvider>
 }
