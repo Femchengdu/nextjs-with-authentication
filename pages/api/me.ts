ef91a1caadc89552d611502e01d8676b9b8906b1
Femi Ajayi <femiajayi705@gmail.com>
2023-01-09 22:29:24 +0100

    feat: Added authenticated api route

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -2,12 +2,6 @@ import { validateRoute } from '../../lib/auth'
 import prisma from '../../lib/prisma'
 
 export default validateRoute(async (req: any, res: any, user: any) => {
-    const playlistsCount = await prisma.playlist.count({
-        where: {
-            userId: user.id,
-        },
-    })
-
-    console.log(playlistsCount)
-    res.json({ ...user, playlistsCount })
+    console.log(user)
+    res.json({ ...user })
 })
